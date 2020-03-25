78c6e980607b5604ae17c6d214ae294bc0f7ba3e
Frederik Mathiesen <frederik_mathies@hotmail.com>
2020-03-24 18:25:04 -0700

    Setup development pipeline for statecharts using sismic

--- siren/consensus/generate_viz.sh
+++ siren/consensus/generate_viz.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
 # Assuming sismic is in the current python scope be that the global site-packages or a virtual environment
-# Takes 1 optional parameter of the name of the PlantUML file (excluding file extension). If no name is specified, the name defaults to consensus.txt.
 
-sismic-plantuml consensus.yaml > ${1:-consensus}.txt
+# Usage: ./generate_viz.sh [-p] [file_name]
+# - p: run PlantUML on the generated file
+# - file_name: name of the PlantUML file (excluding file extension). Default: consensus.
+
+while getopts "p" opt; do
+  case $opt in
+    p ) plantuml=true;;
+    * ) echo "Invalid option"; exit 1;;
+  esac
+
+  shift $((OPTIND -1))
+done
+
+file_name=${1:-consensus}
+
+sismic-plantuml consensus.yaml > "$file_name.txt"
+
+if [ $plantuml ]; then
+  plantuml "$file_name.txt";
+fi
