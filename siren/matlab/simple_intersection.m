05d57dd70716ea407fc669c70ef95a9359e2c419
Garey Fleeman <gareyflee@gmail.com>
2020-04-15 12:55:34 -0700

    Started creating a simple intersection file.

---
+++ siren/matlab/simple_intersection.m
@@ -0,0 +1,21 @@
+%Simple intersection with four lanes and no left turns
+
+%Starting lane states (colors)
+green = [1; 0; 1; 0];
+yellow = [0; 0; 0; 0];
+red = [0; 1; 0; 1];
+amber = [0; 0; 0; 0];
+
+%Conflict matrix for all 4 lanes (no turns in this model)
+conflict_matrix = ...
+[
+    0, 1, 0, 1; 
+    1, 0, 1, 0;
+    0, 1, 0, 1; 
+    1, 0, 1, 0
+];
+
+
+green_interval_matrix = [[0 5] [5 0]];
+yellow_time_vector = [3 3];
+amber_time_vector = [3 3];
