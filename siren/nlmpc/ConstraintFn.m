b94932d859b383e9b830c232b5beb4a619350a86
Frederik Mathiesen <frederik_mathies@hotmail.com>
2020-04-25 17:08:19 -0700

    Ident

--- siren/nlmpc/ConstraintFn.m
+++ siren/nlmpc/ConstraintFn.m
@@ -1,57 +1,57 @@
 function ceq = ConstraintFn(X, U, data, conflict_matrix, green_interval, yellow_time, amber_time, min_green_time)
 
-% Sum of lights = 1
-signals = U(:, 1:4 * num_signals);
-green = signals(:, index(0));
-red = signals(:, index(1));
-yellow = signals(:, index(2));
-amber = signals(:, index(3));
+    % Sum of lights = 1
+    signals = U(:, 1:4 * num_signals);
+    green = signals(:, index(0));
+    red = signals(:, index(1));
+    yellow = signals(:, index(2));
+    amber = signals(:, index(3));
 
-sum_of_lights = green + red + yellow + amber - 1;
+    sum_of_lights = green + red + yellow + amber - 1;
 
-[stacked_lights, size_lights] = mstack(sum_of_lights);
-ceq(1:size_lights) = stacked_lights;
+    [stacked_lights, size_lights] = mstack(sum_of_lights);
+    ceq(1:size_lights) = stacked_lights;
 
-% Pairwise hadamard
-gr = green .* red;
-gy = green .* yellow;
-ga = green .* amber;
-ry = red .* yellow;
-ra = red .* amber;
-ya = yellow .* amber;
+    % Pairwise hadamard
+    gr = green .* red;
+    gy = green .* yellow;
+    ga = green .* amber;
+    ry = red .* yellow;
+    ra = red .* amber;
+    ya = yellow .* amber;
 
-[stacked_pairwise, size_pairwise] = mstack([gr; gy; ga; ry; ra; ya]);
-ceq((1:size_pairwise) + size_lights) = stacked_pairwise;
+    [stacked_pairwise, size_pairwise] = mstack([gr; gy; ga; ry; ra; ya]);
+    ceq((1:size_pairwise) + size_lights) = stacked_pairwise;
 
-% Transition (e.g. from red to amber is allowed) constraints
-p = data.PredictionHorizon;
+    % Transition (e.g. from red to amber is allowed) constraints
+    p = data.PredictionHorizon;
 
-old_idx = 1:p - 1;
-new_idx = 2:p;
+    old_idx = 1:p - 1;
+    new_idx = 2:p;
 
-yt = yellow(old_idx, :) .* (green(new_idx, :) + amber(new_idx, :));
-gt = green(old_idx, :) .* (red(new_idx, :) + amber(new_idx, :));
-at = amber(old_idx, :) .* (red(new_idx, :) + yellow(new_idx, :));
-ryt = red(old_idx, :) .* yellow(new_idx, :);
-rgt = red(old_idx, :) .* green(new_idx, :) .* repmat(amber_time, 1, p - 1).';
-rat = red(old_idx, :) .* amber(new_idx, :) .* (indicator(repmat(amber_time, 1, p - 1).') - 1);
+    yt = yellow(old_idx, :) .* (green(new_idx, :) + amber(new_idx, :));
+    gt = green(old_idx, :) .* (red(new_idx, :) + amber(new_idx, :));
+    at = amber(old_idx, :) .* (red(new_idx, :) + yellow(new_idx, :));
+    ryt = red(old_idx, :) .* yellow(new_idx, :);
+    rgt = red(old_idx, :) .* green(new_idx, :) .* repmat(amber_time, 1, p - 1).';
+    rat = red(old_idx, :) .* amber(new_idx, :) .* (indicator(repmat(amber_time, 1, p - 1).') - 1);
 
-[stacked_trans, size_trans] = mstack([yt; gt; at; ryt; rgt; rat]);
-ceq((1:size_trans) + size_lights + size_pairwise) = stacked_trans;
+    [stacked_trans, size_trans] = mstack([yt; gt; at; ryt; rgt; rat]);
+    ceq((1:size_trans) + size_lights + size_pairwise) = stacked_trans;
 
-% Conflict
-non_blocking = green + yellow + amber;
+    % Conflict
+    non_blocking = green + yellow + amber;
 
-for i = 1:size(non_blocking, 2)
-    ceq(i + size_trans + size_lights + size_pairwise) = non_blocking(i, :) * conflict_matrix * non_blocking(i, :).';
-end
+    for i = 1:size(non_blocking, 2)
+        ceq(i + size_trans + size_lights + size_pairwise) = non_blocking(i, :) * conflict_matrix * non_blocking(i, :).';
+    end
 
 function idx = index(i)
-idx = i * num_signals + 1:(i + 1) * num_signals;
+    idx = i * num_signals + 1:(i + 1) * num_signals;
 
 function [arr_stack, stack_size] = mstack(m)
-arr_stack = reshape(m, [], 1);
-stack_size = size(arr_stack, 1);
+    arr_stack = reshape(m, [], 1);
+    stack_size = size(arr_stack, 1);
 
 function y = indicator(x)
-y = x >= 1;
+    y = x >= 1;
--- siren/nlmpc/StateFn.m
+++ siren/nlmpc/StateFn.m
@@ -1,43 +1,42 @@
 function xk1 = StateFn(xk, uk)
 
+    xk1 = xk;
 
-xk1 = xk;
+    % Times
+    g_idx = index(0);
+    r_idx = index(1);
+    y_idx = index(2);
+    a_idx = index(3);
+    ng_idx = index(4);
 
-% Times
-g_idx = index(0);
-r_idx = index(1);
-y_idx = index(2);
-a_idx = index(3);
-ng_idx = index(4);
+    g = uk(g_idx);
+    r = uk(r_idx);
+    y = uk(y_idx);
+    a = uk(a_idx);
 
-g = uk(g_idx);
-r = uk(r_idx);
-y = uk(y_idx);
-a = uk(a_idx);
+    xk1(g_idx) = (xk(g_idx) + 1) .* g;
+    xk1(r_idx) = (xk(r_idx) + 1) .* r;
+    xk1(y_idx) = (xk(y_idx) + 1) .* y;
+    xk1(a_idx) = (xk(a_idx) + 1) .* a;
+    xk1(ng_idx) = (xk(ng_idx) + 1) .* (1 - g);
 
-xk1(g_idx) = (xk(g_idx) + 1) .* g;
-xk1(r_idx) = (xk(r_idx) + 1) .* r;
-xk1(y_idx) = (xk(y_idx) + 1) .* y;
-xk1(a_idx) = (xk(a_idx) + 1) .* a;
-xk1(ng_idx) = (xk(ng_idx) + 1) .* (1 - g);
+    % Queue params
+    c_a = uk(index(4));
+    c_d = uk(index(5));
+    q_idx = index(5);
+    s_idx = index(6);
+    tq_idx = index(7);
 
-% Queue params
-c_a = uk(index(4));
-c_d = uk(index(5));
-q_idx = index(5);
-s_idx = index(6);
-tq_idx = index(7);
-
-% Replace c_a and c_d with uk(end idxs)
-xk1(q_idx) = rectifier(xk(q_idx) + c_a - c_d .* (g + y));
-xk1(s_idx) = xk(s_idx) + c_a .* (r + a);
-xk1(tq_idx) = (xk(tq_idx) + 1) .* indicator((1 - g) .* xk1(q_idx));
+    % Replace c_a and c_d with uk(end idxs)
+    xk1(q_idx) = rectifier(xk(q_idx) + c_a - c_d .* (g + y));
+    xk1(s_idx) = xk(s_idx) + c_a .* (r + a);
+    xk1(tq_idx) = (xk(tq_idx) + 1) .* indicator((1 - g) .* xk1(q_idx));
 
 function idx = index(i)
-idx = i * num_signals + 1:(i + 1) * num_signals;
+    idx = i * num_signals + 1:(i + 1) * num_signals;
 
 function y = indicator(x)
-y = x >= 1;
+    y = x >= 1;
 
 function y = rectifier(x)
-y = max(x, zeros(size(x)));
+    y = max(x, zeros(size(x)));
