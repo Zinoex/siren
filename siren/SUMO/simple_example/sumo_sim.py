a3f3abae458a4e13e9cf691c5591475867f6598d
Frederik Mathiesen <frederik_mathies@hotmail.com>
2020-05-01 12:26:48 -0700

    Format code to satisfy PEP8

--- siren/SUMO/simple_example/sumo_sim.py
+++ siren/SUMO/simple_example/sumo_sim.py
@@ -1,40 +1,40 @@
-import os 
+import os
 import sys
 import optparse
-import random
 
 from sumolib import checkBinary
 import traci
 
 
-
-
-class SUMO_Simulation:
-    def __init__(self, max_iterations = 100, config_file = './data/cross.sumocfg',gui_bin_loc = "/usr/bin/sumo-gui"):
+class SUMOSimulation:
+    def __init__(self, max_iterations=100, config_file='./data/cross.sumocfg', gui_bin_loc="/usr/bin/sumo-gui"):
         self.simulation_step = 0
         self.max_iterations = max_iterations
+
         if "SUMO_HOME" in os.environ:
             tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
             sys.path.append(tools)
         else:
             sys.exit("SUMO_HOME not declared in path.")
-        optParser = optparse.OptionParser()
-        optParser.add_option("--nogui", action="store_true",
-                            default=False, help="run the commandline version of sumo")
-        self.options, self.option_args = optParser.parse_args()
+        opt_parser = optparse.OptionParser()
+        opt_parser.add_option("--nogui", action="store_true",
+                             default=False, help="run the commandline version of sumo")
+
+        self.options, self.option_args = opt_parser.parse_args()
         self.sumoCmd = [gui_bin_loc, "-c", config_file]
+
         if self.options.nogui:
             self.sumoBinary = checkBinary("sumo")
             print("No GUI detected.")
         else:
             self.sumoBinary = checkBinary("sumo-gui")
             print("GUI detected.")
-    
+
     def start_sim(self):
         print("Starting Sumo Simulation.")
         traci.start(self.sumoCmd)
         traci.trafficlight.setPhase("0", 2)
-        
+
     def step_sim(self):
         if self.simulation_step <= self.max_iterations:
             traci.simulationStep()
@@ -48,5 +48,6 @@ class SUMO_Simulation:
 
     def update_lights(self):
         pass
-    def get_traffic_info():
+
+    def get_traffic_info(self):
         pass
